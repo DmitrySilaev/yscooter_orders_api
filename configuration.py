9e3bddfcba540b1b668ff548075798b2f3419b78
Dmitriy Silaev <s91097221699@gmail.com>
2023-09-20 14:03:29 +0300

    09/20/2023 project version 1

---
+++ configuration.py
@@ -0,0 +1,8 @@
+# Тестовый стенд веб-приложения Яндекс Cамокат
+URL_SERVICE = "https://20edc596-6183-47e6-a298-57edf29a87a1.serverhub.praktikum-services.ru"
+
+# POST API Orders - Создание заказа
+CREATE_ORDER = "/api/v1/orders"
+
+# GET API Orders - Получить заказ по его номеру
+GET_ORDER = "/api/v1/orders/track"
